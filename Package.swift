f253523f23a7d531a4f172c91301d185bb4eb562
Datt <19645535+Datt1994@users.noreply.github.com>
2026-06-05 16:01:24 -0700

    SPM

--- Package.swift
+++ Package.swift
@@ -1,15 +1,21 @@
-// swift-tools-version:5.0
-//
-//  Package.swift
-//
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-let package = Package(name: "DPCoordinator",
-                      platforms: [.iOS(.v18)],
-                      products: [.library(name: "DPCoordinator",
-                                          targets: ["DPCoordinator"])],
-                      targets: [.target(name: "DPCoordinator",
-                                        path: "DPCoordinator/Source",
-                                        publicHeadersPath: "")],
-                      swiftLanguageVersions: [.v5])
+let package = Package(
+    name: "DPCoordinator",
+    platforms: [.iOS(.v17)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DPCoordinator",
+            targets: ["DPCoordinator"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "DPCoordinator"),
+    ],
+)
